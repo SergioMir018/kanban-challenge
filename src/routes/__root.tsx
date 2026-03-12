894293e21b1c0ab80502232390c54330a3ec97f0
SergioMir018 <sergiommirs@gmail.com>
2026-03-12 01:28:02 -0400

    refactor: removed boilerplate

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,9 +5,6 @@ import {
 } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-
 import TanStackQueryProvider from '../integrations/tanstack-query/root-provider'
 
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
@@ -53,11 +50,9 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <script dangerouslySetInnerHTML={{ __html: THEME_INIT_SCRIPT }} />
         <HeadContent />
       </head>
-      <body className="font-sans antialiased [overflow-wrap:anywhere] selection:bg-[rgba(79,184,178,0.24)]">
+      <body>
         <TanStackQueryProvider>
-          <Header />
           {children}
-          <Footer />
           <TanStackDevtools
             config={{
               position: 'bottom-right',
